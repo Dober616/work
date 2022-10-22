a482fbe03ea339a7bf8e5eaa0eadea70972a07bd
Dober616 <Druz_kirill@icloud.com>
2022-10-22 23:08:46 +0200

    Решили большее меньшее число

--- module 15.2.py
+++ module 15.2.py
@@ -1,1 +1,14 @@
-#Задача 1. гугл
+nums_list = []
+n = int(input('Введите количество чисел в списке: '))
+for _ in range (n):
+    numm = int(input('Введите число: '))
+    nums_list.append(numm)
+maximum  = nums_list[1]
+minimum  = nums_list[1]
+for i in nums_list:
+    if i >= maximum:
+        maximum = i
+    elif i < minimum:
+        minimum = i
+print('Максимальное число: ', maximum)
+print('Минимальное число: ', minimum)
