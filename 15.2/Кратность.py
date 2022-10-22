26cbab3f994797231a016671b1727a9d24034c91
Dober616 <Druz_kirill@icloud.com>
2022-10-22 23:45:24 +0200

    Решили большее меньшее число

--- 15.2/Кратность.py
+++ 15.2/Кратность.py
@@ -1,4 +1,7 @@
 numm_list = []
 n = int(input('Введите количество чисел: '))
 for i in range (n):
-    number = int(input('Введите ', i+1, 'число: '))
+    number = int(input('Введите число: '))
+    numm_list.append(i+1)
+print(numm_list)
+deliver = int(input('Введите делитель: '))
