3e1b383a74d3301018a09ea714f057c57f6d655b
Dober616 <Druz_kirill@icloud.com>
2022-10-23 00:47:57 +0200

    Выполнили задание делитель

--- 15.2/Кратность.py
+++ 15.2/Кратность.py
@@ -2,6 +2,15 @@ numm_list = []
 n = int(input('Введите количество чисел: '))
 for i in range (n):
     number = int(input('Введите число: '))
-    numm_list.append(i+1)
+    numm_list.append(number)
+print()
 print(numm_list)
+print()
 deliver = int(input('Введите делитель: '))
+summ = 0
+x = -1
+for y in numm_list:
+    if y % deliver == 0:
+        print('Индекс числа ', y,  ':', numm_list.index(y))
+        summ += numm_list.index(y)
+print('Сумма индексов =', summ)
