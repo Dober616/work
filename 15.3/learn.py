8343b348ca854d9a2fa739baabaa0140d938e2b5
Dober616 <Druz_kirill@icloud.com>
2022-10-23 14:35:14 +0200

    Про слова доделал

--- 15.3/learn.py
+++ 15.3/learn.py
@@ -1,27 +1,17 @@
-text = input('Введите строку: ')
-sym_numm = int(input('Номер символа: ')) -1
-letters = list(text)
-count = 0
-print('Длина текста', len(text))
-for symbol in text:
-    if symbol == text[sym_numm]:
-        count += 1
-if sym_numm == len(text)-1:
-    print ('Справа символов не будет')
-elif sym_numm >len(text)-1:
-    print ('Вы вообще вышли за рамки')
-else:
-    print('Символ справа: \t', letters[sym_numm + 1])
-if sym_numm == 0:
-    print('Слева символов не будет')
-elif sym_numm < 0:
-    print ('Вы вообще вышли за рамки')
-else:
-    print('Символ слева: \t', letters[sym_numm-1])
+words = []
+counts = [0, 0, 0]
+for i in range(3):
+    print('Введите ', i+1, 'слово: ', end='')
+    word = input()
+    words.append(word)
+print(words)
+text = input('Слово из текста: ')
+while text != 'end':
+    for index in range (3):
+        if words[index] == text:
+            counts [index] += 1
+    text = input('Слово из текста: ')
 
-if count == 0:
-    print('Таких же символов нет')
-elif count == 1:
-    print('Есть ровно один такой символ')
-else:
-    print('Таких символов: ', count)
+print('Подсчет слов в тексте: ')
+for t in range(3):
+    print(words[t], ':\t', counts[t])
