662b2ca821f7caf15962ad2c573bae8c448b6542
Dober616 <Druz_kirill@icloud.com>
2022-10-23 13:53:18 +0200

    осталось добавить условие если выбрал последний или первый знак

--- 15.3/learn.py
+++ 15.3/learn.py
@@ -1,6 +1,17 @@
-words_list = []
-counts = [0, 0, 0]
-for i in range (3):
-    print('Введите ', i + 1, 'слово: ', end='')
-    word = input()
-    words_list.append(word)
+text = input('Введите строку: ')
+sym_numm = int(input('Номер символа: ')) -1
+letters = list(text)
+count = 0
+
+for symbol in text:
+    if symbol == text[sym_numm]:
+        count += 1
+print('Символ слева: \t', letters[sym_numm-1])
+print('Символ справа: \t', letters[sym_numm+1])
+
+if count == 0:
+    print('Таких же символов нет')
+elif count == 1:
+    print('Есть ровно один такой же символ')
+else:
+    print('Таких символов: ', count)
