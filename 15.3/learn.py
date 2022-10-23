3e4091da117e08ac0827ff65f7ada37a9029cf73
Dober616 <Druz_kirill@icloud.com>
2022-10-23 15:08:47 +0200

    конец модуля

--- 15.3/learn.py
+++ 15.3/learn.py
@@ -1,17 +1,7 @@
-words = []
-counts = [0, 0, 0]
-for i in range(3):
-    print('Введите ', i+1, 'слово: ', end='')
-    word = input()
-    words.append(word)
-print(words)
-text = input('Слово из текста: ')
-while text != 'end':
-    for index in range (3):
-        if words[index] == text:
-            counts [index] += 1
-    text = input('Слово из текста: ')
 
-print('Подсчет слов в тексте: ')
-for t in range(3):
-    print(words[t], ':\t', counts[t])
+
+scores = [8, 5, 10, 7, 6]
+scores[1] += len(scores)
+scores.append(0)
+scores[3] += len(scores)
+print(scores)
