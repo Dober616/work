e9229fcb264d8189e235f80fc072fefc012b831e
druz_kirill <Druz_kirill@icloud.com>
2022-11-07 11:35:19 +0200

    10 вроде работает

--- try.py
+++ try.py
@@ -1,27 +1,17 @@
-def drop_number():
-    number = count_number-1 - len(people_round)
-    return number
-
-people_count = 5#int(input('Количество человек: '))
-count_number = 7#int(input('Какое число в считалке: '))
-people_round = []
-print('Значит выбывает каждый', count_number, '- й человек', '\n++++++++++++++++++++++++++++++++++++')
-for i in range(1, people_count+1):
-    people_round.append(i)
-print(people_round)
-
-number = drop_number()
-
-while len(people_round) != 1:
-    if number > len(people_round):
-        number -= len(people_round)
-    people_round.pop(number)
-
-    print(people_round)
-
-
-
-print(people_round)
-
+count_numbers = int(input('Количество чисел: '))
+subsequence = []
+for _ in range(count_numbers):
+    number = int(input('Число: '))
+    subsequence.append(number)
+add_list = []
+y = 1
+for digit in subsequence:
+    last_numm = subsequence[-y]
+    if digit == last_numm:
+        y += 1
+    else:
+        add_list.insert(0, digit)
+if len(add_list) > 0:
+    print('Последовательность: ', subsequence, '\nНужно приписать чисел: ', len(add_list), '\nСами числа: ', add_list)
 
 
