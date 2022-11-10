889cbcc9a320e94b0c3e978d43e812b75a45562c
druz_kirill <Druz_kirill@icloud.com>
2022-11-10 12:07:59 +0200

    6 залепуха, но работает

--- try.py
+++ try.py
@@ -1,18 +1,4 @@
+a = [1, 2, 3, 4]
+a[-1:] = [2,2,2]
 
-
-first_list = [1, 2, 3]
-sec_list = [4, 6, 3, 2, 1]
-first_list.extend(sec_list)
-print(first_list)
-
-for i in range(1, len(first_list)):
-
-    first_list.remove(first_list[-i])
-    print(first_list)
-
-print('Новый список с уникальными эелементами:', first_list)
-
-#
-# list = ['a','b','c','c', 'd','e']
-# list.remove('c')
-# print(list)
+print(a)
