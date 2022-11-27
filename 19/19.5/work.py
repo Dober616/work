38c418292e1a896f3de1b74fc059e8df2aa89f1e
Dober616 <Druz_kirill@icloud.com>
2022-11-27 13:19:06 +0200

    19.5

---
+++ 19/19.5/work.py
@@ -0,0 +1,16 @@
+data = [
+    {'id': 10, 'user': 'Bob'},
+    {'id': 11, 'user': 'Misha'},
+    {'id': 12, 'user': 'Anton'},
+    {'id': 10, 'user': 'Bob'},
+    {'id': 11, 'user': 'Misha'}
+]
+unique_data = []
+# for i in data:
+#     if i in unique_data:
+#         pass
+#     else:
+#         unique_data.append(i)
+
+unique_data_dict = {user['id']: user for user in data}
+print(unique_data_dict.values())
