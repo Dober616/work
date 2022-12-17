a49c3d510c9c70b2749502b98b6640a7699281d0
druz_kirill <Druz_kirill@icloud.com>
2022-12-17 17:28:23 +0200

    21.6

--- 21/21.6/3. Помощь другу.py
+++ 21/21.6/3. Помощь другу.py
@@ -1,21 +1,22 @@
-def create_dict(data):
-    template = {}
+def create_dict(data, template=None):
     if isinstance(data, dict):
         return data
-    elif isinstance(data, int) or isinstance(data, str) or isinstance(data, float):
+    elif isinstance(data, (int, float, str)):
+        template = template or dict()
         template[data] = data
         return template
 
 
 my_string = ["sad", {"sds": 23}, {43}, [12, 42, 1], 2323]
-new_list = []
 
 
-def data_preparation(new_list):
+
+def data_preparation():
+    new_list = []
     for element in my_string:
         if create_dict(element):
             new_list.append(create_dict(element))
     return new_list
 
 
-print(data_preparation(new_list))
+print(data_preparation())
