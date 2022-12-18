5087ef47486e7eb34ce6981e2efbdc019fd89f8f
druz_kirill <Druz_kirill@icloud.com>
2022-12-18 12:25:40 +0200

    1 готово.

--- 21/21.2/work.py
+++ 21/21.2/work.py
@@ -1,42 +1,38 @@
-# def factorial(number):
-#     if number == 1:
-#         return 1
-#     result = number * factorial(number - 1)
+def factorial(some_numm):
+    if some_numm == 1:
+        return 1
+    fact = factorial(some_numm -1)
+    return some_numm * fact
+print(factorial(5))
+# site ={
+#     'html': {
+#         'head': {
+#             'title': 'Мой сайт'
+#         },
+#         'body': {
+#             'h2': 'Здесь будет мой заголовок',
+#             'div': 'Тут, наверное, какой-то блок',
+#             'p': 'А вот здесь новый абзац'
+#         }
+#     }
+# }
+# def find_key(structure, key):
+#     if key in structure:
+#         return structure[key]
+#
+#     for sub_struct in structure.values():
+#         if isinstance(sub_struct, dict):
+#             result = find_key(sub_struct, key)
+#             if result:
+#                 break
+#     else:
+#         result = None
 #     return result
 #
 #
-# num_fact = factorial(5)
-# print(f'Факториал равен {num_fact}')
-
-site ={
-    'html': {
-        'head': {
-            'title': 'Мой сайт'
-        },
-        'body': {
-            'h2': 'Здесь будет мой заголовок',
-            'div': 'Тут, наверное, какой-то блок',
-            'p': 'А вот здесь новый абзац'
-        }
-    }
-}
-def find_key(structure, key):
-    if key in structure:
-        return structure[key]
-
-    for sub_struct in structure.values():
-        if isinstance(sub_struct, dict):
-            result = find_key(sub_struct, key)
-            if result:
-                break
-    else:
-        result = None
-    return result
-
-
-user_key = input('Какой ключ ищем? ')
-value = find_key(site, user_key)
-if value:
-    print(value)
-else:
-    print('Такого ключа в структуре сайта нет.')
+# user_key = input('Какой ключ ищем? ')
+# value = find_key(site, user_key)
+# if value:
+#     print(value)
+# else:
+#     print('Такого ключа в структуре сайта нет.')
