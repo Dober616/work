6733129ab1714b5bed333ac18a2dd0a7ea216ce6
druz_kirill <Druz_kirill@icloud.com>
2022-12-22 10:05:33 +0200

    4 нагородил конечно, но вроде работает

--- 21/21.2/work.py
+++ 21/21.2/work.py
@@ -1,41 +1,40 @@
-# def factorial(num):
-#     if num == 1:
+# def fact(num):
+#     if num == 0:
 #         return 1
-#     fact_minus_1 = factorial(num - 1)
-#     return num * fact_minus_1
-#
-#
-# num_fact = factorial(5)
-# print(num_fact)
-def find_key_value(data, some_key):
-    if some_key in data:
-        return data[some_key]
-    for subsruct in data.values():
-        if isinstance(subsruct, dict):
-            result =  find_key_value(subsruct, some_key)
-            if result:
-                break
-    else:
-        result = None
-    return result
+#     factorial = num * fact(num - 1)
+#     return factorial
+# print(fact(5))
+
 
 site = {
     'html': {
         'head': {
-            'title': 'Мой сайт'
+            'title': 'Мой сайт',
         },
         'body': {
-            'h2': 'Здесь будет мой загловок',
+            'h2': 'Здесь будет мой заголовок',
             'div': 'Тут, наверное, какой-то блок',
             'p': 'А вот здесь новый абзац'
         }
     }
 }
 
-my_key = input('Введите искомый ключ: ')
+def find_key(struct, some_key):
+    if some_key in struct:
+        return struct[some_key]
+    for substruct in struct.values():
+        if isinstance(substruct, dict):
+            result = find_key(substruct, some_key)
+            if result:
+                break
+    else:
+        result = None
+    return result
 
-value = find_key_value(site, my_key)
+
+user_key = input('Какой ключ ищем? ')
+value = find_key(site, user_key)
 if value:
     print(value)
 else:
-    print('Такого значения в структуре нет')
+    print('Такого ключа в нашей структуре нет')
--- 21/21.3/work.py
+++
@@ -1,9 +0,0 @@
-def change_values(some_list):
-    for i_index, i_value in enumerate(some_list):
-        some_list[i_index] += 3
-
-
-my_list = [10, 20, 30]
-number = 100
-change_values(my_list)
-print(my_list)
