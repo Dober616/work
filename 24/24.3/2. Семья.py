83e0ad7bfbe5970d69a3770cacb6b3fccdc4d837
druz_kirill <Druz_kirill@icloud.com>
2023-01-15 17:17:30 +0200

    work

--- 24/24.3/2. Семья.py
+++ 24/24.3/2. Семья.py
@@ -8,12 +8,13 @@ class Family:
               f'Сбережения семьи: {self.family_founds}\n'
               f'Наличие дома: {self.having_house}'
               )
+
     def get_money(self, summ):
         self.family_founds += summ
         print(f'Заработано денег: {summ}! Текущее количество денег: {self.family_founds}')
 
-    def buy_house(self, price, discount=0):
-        house_price = price - price * (discount / 100)
+    def buy_house(self, my_price, discount=0):
+        house_price = my_price - my_price * (discount / 100)
         if self.family_founds >= house_price:
             print(f'Дом куплен за {house_price}, остаток средств {self.family_founds}')
             self.having_house = True
