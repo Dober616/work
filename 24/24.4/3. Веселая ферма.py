dd5fac800cd505e5c5e830debf78648021787dec
druz_kirill <Druz_kirill@icloud.com>
2023-01-18 11:48:21 +0200

    work

--- 24/24.4/3. Веселая ферма.py
+++ 24/24.4/3. Веселая ферма.py
@@ -1,45 +1,4 @@
-class Potato:
-    states = {0: 'Отсутствует', 1: 'Росток', 2: 'Зеленая', 3: 'Спелая'}
-    def __init__(self, index):
-        self.index = index
-        self.states = 0
-
-    def grow(self):
-        if self.states < 3:
-            self.states += 1
-        self.print_state()
-
-
-    def is_ripe(self):
-        if self.states == 3:
-            return True
-
-    def print_state(self):
-        print(f'картошка {self.index} сейчас {Potato.states[self.states]}')
-
-
-
-
-class Garden:
-    def __init__(self, count):
-        self.potatoes = [Potato(index) for index in range(1, count + 1)]
-
-    def grow_all(self):
-        print('Картошка прорастает!')
-        for potato in self.potatoes:
-            potato.grow()
-
-    def are_all_ripe(self):
-        if not all(potato.is_ripe() for potato in self.potatoes):
-            print('Картошка еще не созрела')
-            return False
-        else:
-            print('Вся картошка созрела')
-            return True
-
-    def print_all_states(self):
-        for potato in self.potatoes:
-            potato.print_state()
+from garden import Garden
 
 garden = Garden(5)
 for _ in range(3):
