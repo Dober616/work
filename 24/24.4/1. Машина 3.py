bc3b75a0ad51ed3aba93ea9c84184759a857e67e
druz_kirill <Druz_kirill@icloud.com>
2023-01-22 20:49:51 +0200

    9 вобщем как-то так

--- 24/24.4/1. Машина 3.py
+++ 24/24.4/1. Машина 3.py
@@ -1,21 +1,13 @@
-class Car():
+class Car:
 
     def __init__(self, color, price, max_speed, current_speed):
         self.color = color
         self.price = price
         self.max_speed = max_speed
-        self.current_speed = int(input('Сами введите текущую скорость: '))
+        self.current_speed = current_speed
 
     def info(self):
-        print(f'Цвет: {self.color}\n'
-              f'цена: {self.price}\n'
-              f'макс.скор: {self.max_speed}\n'
-              f'тек.скор: {self.current_speed}')
+        print(f'{self.color}, {self.price}, {self.max_speed}, {self.current_speed}')
 
-toyota = Car('red', 1000000, 200, 0)
+toyota = Car('red', 100000, 200, 0)
 toyota.info()
-
-bmw = Car('black', 3000000, 250, 0)
-bmw.info()
-
-
--- 24/24.4/work2.py
+++ 24/24.4/work2.py
@@ -1,10 +1,44 @@
 class Potato:
+
+    states = {0: 'Отсутствует', 1: 'Росток', 2: 'Зеленая', 3: 'Спелая'}
     def __init__(self, index):
         self.index = index
         self.state = 0
 
+    def grow(self):
+        if self.state < 3:
+            self.state += 1
+        self.print_state()
+
+    def print_state(self):
+        print(f'Картошка {self.index} сейчас {Potato.states[self.state]}')
+
+    def is_ripe(self):
+        if self.state == 3:
+            return True
+        return False
 
 
 class Garden:
+
     def __init__(self, count):
         self.potatoes = [Potato(index) for index in range(1, count + 1)]
+
+    def grow_all(self):
+        print('Картошка растет!')
+        for some_potato in self.potatoes:
+            some_potato.grow()
+
+    def are_all_right(self):
+        for some_potato in self.potatoes:
+            if not some_potato.is_ripe():
+                print('Картошка еще не созрела')
+                self.grow_all()
+                break
+            else:
+                print('Вся картошка созрела, можно собирать')
+
+
+my_garden = Garden(5)
+for _ in range(3):
+    my_garden.are_all_right()
