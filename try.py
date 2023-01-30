c2b607d89924fde24b9dc7990b890d752db22626
druz_kirill <Druz_kirill@icloud.com>
2023-01-30 15:13:29 +0200

    4 готово, вроде все работает

--- try.py
+++ try.py
@@ -1,48 +1,104 @@
-class Property:
-    def __init__(self, worth=0, tax=0):
-        self.set_worth(worth)
-        self.set_tax(tax)
+class Person:
+    def __init__(self, name, surname, age):
+        self.__name = name
+        self.__surname = surname
+        self.__age = age
 
-    def get_tax(self):
-        return self.tax
+    def __str__(self):
+        return f'Имя: {self.__name}, фамилия: {self.__surname}, ' \
+               f'возраст: {self.__age}'
 
-    def set_tax(self, tax):
-        self.tax = self.worth * tax / 100
-
-    def get_worth(self):
-        return self.worth
-
-    def set_worth(self, worth):
-        self.worth = int(input(f'Ввведите стоимость: '))
-
-    def get_summ(self):
-        return self.get_worth() + self.get_tax()
+    def get_name(self):
+        return self.__name
 
 
-class Apartment(Property):
-    def __init__(self):
-        super().__init__()
-        self.set_tax(0.1)
+
+class Emploee(Person):
+    def __init__(self, name, surname, age, bet=0, percent=0, hour=0):
+        super().__init__(name, surname, age)
+        self.bet = bet
+        self.percent = percent
+        self.hour = hour
+        self.set_salary()
+
+    def set_salary(self):
+        self.salary = self.bet + (self.percent * 5 / 100) + (self.hour * 100)
+
+    def get_salary(self):
+        return self.salary
 
 
-class Car(Property):
-    def __init__(self):
-        super().__init__()
-        self.set_tax(0.5)
+class Manager(Emploee):
+    def __init__(self, name, surname, age, bet=0):
+        super().__init__(name, surname, age)
+        self.set_bet(bet)
+
+    def set_bet(self, bet):
+        bet = int(input(f'Введите оклад {self.get_name()}: '))
+        if bet > 0:
+            self.bet = bet
+        else:
+            raise Exception('Оклад не может быть отрицательным')
+
+    def get_bet(self):
+        return self.bet
+
+    def get_salary(self):
+        return self.bet
+
+    def __str__(self):
+        info = super().__str__()
+        info = f'{info}, зарплата: {self.get_salary()}'
+        return info
 
 
-class CountryHouse(Property):
-    def __init__(self):
-        super().__init__()
-        self.set_tax(0.2)
+class Agent(Emploee):
+    def __init__(self, name, surname, age, bet=5000, percent=0):
+        super().__init__(name, surname, age)
+        self.bet = bet
+        self.set_percent()
 
-my_flat = Apartment()
-my_car = Car()
-my_house = CountryHouse()
 
-general = my_flat.get_summ() + my_car.get_summ() + my_house.get_summ()
-my_money = int(input('Сколько денег то у вас? '))
-if my_money >= general:
-    print('Поздравляем, вы можете все это купить')
-else:
-    print('Надо бы еще подкопить')
+
+    def set_percent(self):
+        percent = int(input(f'Какая выручка у {self.get_name()}?: '))
+        self.percent = percent * 5 / 100
+
+    def get_percent(self):
+        return self.percent
+
+    def get_salary(self):
+        return self.bet + self.percent
+
+    def __str__(self):
+        info = super().__str__()
+        info = f'{info}, зарплата: {self.get_salary()}'
+        return info
+
+
+class Worker(Emploee):
+    def __init__(self, name, surname, age, hours=0):
+        super().__init__(name, surname, age)
+        self.set_hours()
+
+    def set_hours(self):
+        hours = int(input(f'Сколько часов отработал {self.get_name()}?: '))
+        self.hour = hours
+
+    def get_salary(self):
+        return self.hour * 100
+
+    def __str__(self):
+        info = super().__str__()
+        info = f'{info}, зарплата: {self.get_salary()}'
+        return info
+
+
+kirill = Manager('Кирилл', 'Друзьяк', 38)
+print(kirill)
+
+egor = Agent('Егор', 'Сергеев', 2)
+print(egor)
+
+jamshud = Worker('Джамшуд', 'Нуриев', 45)
+print(jamshud)
